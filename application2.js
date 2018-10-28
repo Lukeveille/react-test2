6f31f7c0d6767df67fef5d81ada1aafea33ef8b3
Lukeveille <luke@lukeleveille.com>
2018-10-28 16:17:11 -0400

    set up first event handler

---
+++ application2.js
@@ -0,0 +1,21 @@
+class Counter extends React.Component{
+  constructor(props){
+    super(props)
+    //initial state set up
+    this.state = {message:"initial message"}
+  }
+  componentDidMount() {
+    //updating state
+    this.setState((prevState, props) => {
+      return {message: prevState.message + '!'}
+    })
+  }
+  render(){
+    return <div>Message: {this.state.message}</div>
+  }
+}
+
+ReactDOM.render(
+  <Counter/>,
+  document.getElementById("root")
+)
