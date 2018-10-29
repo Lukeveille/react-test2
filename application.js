a63409d1a9e74e368b88e889a8ac40297bc16c3c
Lukeveille <luke@lukeleveille.com>
2018-10-29 13:12:49 -0400

    small css revisions

--- application.js
+++ application.js
@@ -1,44 +1,45 @@
 // Setup board.
-function Circle(props){
-  var color = "white"
-  if (props.cell == 1){
-    color = "black"
-  } else if (props.cell == 2){
-    color = "red"
+function Circle(props) {
+  var color = "#FFF"
+  if (props.cell === 1){
+    color = "#000"
+  } else if (props.cell === 2) {
+    color = "#F33"
   }
   var style = {
     backgroundColor: color,
-    border: "1px solid black",
+    border: "1px solid #000",
     borderRadius: "100%",
     paddingTop: "98%"
   }
   return (
-     <div style = {style}></div>
+    <div className = "circle" style = {style}></div>
   )
 }
-function Cell(props){
+function Cell(props) {
   var style = {
     height: 70,
     width: 70,
-    border: "1px solid black",
+    border: "1px solid #000",
     backgroundColor: "yellow"
   }
   return (
-    <div style = {style} onClick = {() => props.handleClick(props.row, props.col)}>
+    <div style = {style} onClick = {() => props.handleClick(props.row,props.col)}>
         <Circle cell = {props.cell}/>
     </div>
   );
 }
 function Row(props) {
   var style = {
-    display: "flex"
+    display: "flex",
+    justifyContent: "center"
   }
   var cells = [];
   for (let i = 0; i < 7; i++) {
     cells.push(<Cell key = {i} cell = {props.cells[i]} row = {props.row} col = {i} handleClick = {props.handleClick}/>)
   }
   return (
-    <div style={style}>
+    <div className='gameRow' style={style}>
       {cells}
     </div>
   );
@@ -82,43 +83,43 @@ class Game extends React.Component {
     return -1;
   }
   // Victory logic.
-  // checkDiagonal(row,col){
-  //   //find right and left tops
-  //   var c = this.state.cells;
-  //   var val = this.state.player? 2:1;
-  //   var rR = row;
-  //   var cR = col;
+  checkDiagonal(row,col){
+    //find right and left tops
+    var c = this.state.cells;
+    var val = this.state.player? 2:1;
+    var rR = row;
+    var cR = col;
     
-  //   while(rR < 5 && cR < 6){
-  //     rR++; 
-  //     cR++;
-  //   }
+    while(rR < 5 && cR < 6){
+      rR++; 
+      cR++;
+    }
 
-  //   while( rR >= 3 && cR >= 3){
-  //     if(c[rR][cR] == val && c[rR-1][cR-1] == val && c[rR-2][cR-2] == val && c[rR-3][cR-3] == val){
-  //       return 1
-  //     }
-  //     rR--
-  //     cR--
-  //   }   
+    while( rR >= 3 && cR >= 3){
+      if(c[rR][cR] === val && c[rR-1][cR-1] === val && c[rR-2][cR-2] === val && c[rR-3][cR-3] === val){
+        return 1
+      }
+      rR--
+      cR--
+    }   
 
-  //   var rL = row;
-  //   var cL = col;
+    var rL = row;
+    var cL = col;
 
-  //   while(rL < 5 && cL > 0){
-  //     rL++
-  //     cL--
-  //   }
+    while(rL < 5 && cL > 0){
+      rL++
+      cL--
+    }
 
-  //   while(rL >= 3 && cL <= 3){
-  //     if(c[rL][cL] == val && c[rL-1][cL+1] == val && c[rL-2][cL+2] == val && c[rL-3][cL+3] == val){
-  //       return 1;
-  //     }
-  //     rL--
-  //     cL++
-  //   }
-  //   return 0;
-  // }
+    while(rL >= 3 && cL <= 3){
+      if(c[rL][cL] === val && c[rL-1][cL+1] === val && c[rL-2][cL+2] === val && c[rL-3][cL+3] === val){
+        return 1;
+      }
+      rL--
+      cL++
+    }
+    return 0;
+  }
   checkHorizontal(row,col){
     var c = this.state.cells;
     var i = col;
@@ -142,11 +143,12 @@ class Game extends React.Component {
       if (c[i][col] == val && c[i+1][col] == val && c[i+2][col] == val && c[i+3][col] == val) {
         return 1
       }
+      i--
     }
     return 0
   }
   checkVictory(row,col){
-    return this.checkVertical(row,col) || this.checkHorizontal(row,col)// || this.checkDiagonal(row,col);
+    return this.checkVertical(row,col) || this.checkHorizontal(row,col) || this.checkDiagonal(row,col);
   }
 
   // Controls.
@@ -176,12 +178,13 @@ class Game extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1>{this.state.winner > 0 ?  this.state.winner == 1? "Black Wins":"Red Wins": this.state.player? "Blacks Turn" : "Reds Turn"} </h1>
+      <div id='gameBoard'>
+        <h1>{this.state.winner > 0?  this.state.winner === 1? "Black Wins" : "Red Wins" : this.state.player? "Blacks Turn" : "Reds Turn"} </h1>
         <Board cells={this.state.cells} handleClick={this.handleClick}/>
         <button onClick = { () => this.restart()}>Restart</button>
       </div>
     )
   }
 }
+
 ReactDOM.render(<Game />, document.getElementById('root'));
