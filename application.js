6c1a3540b8f738ebe8c1e57b6c096ab5d966c7ce
Lukeveille <luke@lukeleveille.com>
2018-10-30 00:42:38 -0400

    cleaned up punctuation

--- application.js
+++ application.js
@@ -1,16 +1,16 @@
 // Setup board.
 function Circle(props) {
-  var color = "#FFF"
-  if (props.cell === 1){
-    color = "#000"
+  var color = "#FFF";
+  if (props.cell === 1) {
+    color = "#000";
   } else if (props.cell === 2) {
-    color = "#F33"
+    color = "#F33";
   }
   var style = {
     backgroundColor: color,
     border: "1px solid #000",
     borderRadius: "100%",
-    paddingTop: "98%"
+    paddingTop: "98%",
   }
   return (
     <div className = "circle" style = {style}></div>
@@ -21,7 +21,7 @@ function Cell(props) {
     height: 70,
     width: 70,
     border: "1px solid #000",
-    backgroundColor: "yellow"
+    backgroundColor: "yellow",
   }
   return (
     <div style = {style} onClick = {() => props.handleClick(props.row,props.col)}>
@@ -32,7 +32,7 @@ function Cell(props) {
 function Row(props) {
   var style = {
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
   }
   var cells = [];
   for (let i = 0; i < 7; i++) {
@@ -61,16 +61,16 @@ class Game extends React.Component {
 
     var cells = []
     for (let i = 0; i < 7; i++ ) {
-      cells.push(new Array(7).fill(0))
+      cells.push(new Array(7).fill(0));
     }
 
     this.state = {
       player: false,
       cells: cells,
-      winner: 0
+      winner: 0,
     }
 
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
   }
   
   // Game logic.
@@ -83,57 +83,57 @@ class Game extends React.Component {
     return -1;
   }
   // Victory logic.
-  checkDiagonal(row,col){
+  checkDiagonal(row,col) {
     //find right and left tops
     var c = this.state.cells;
     var val = this.state.player? 2:1;
     var rR = row;
     var cR = col;
     
-    while (rR < 5 && cR < 6){
+    while (rR < 5 && cR < 6) {
       rR++; 
       cR++;
     }
 
-    while (rR >= 3 && cR >= 3){
-      if (c[rR][cR] === val && c[rR-1][cR-1] === val && c[rR-2][cR-2] === val && c[rR-3][cR-3] === val){
+    while (rR >= 3 && cR >= 3) {
+      if (c[rR][cR] === val && c[rR-1][cR-1] === val && c[rR-2][cR-2] === val && c[rR-3][cR-3] === val) {
         return 1
       }
-      rR--
-      cR--
+      rR--;
+      cR--;
     }   
 
     var rL = row;
     var cL = col;
 
-    while (rL < 5 && cL > 0){
-      rL++
-      cL--
+    while (rL < 5 && cL > 0) {
+      rL++;
+      cL--;
     }
 
-    while (rL >= 3 && cL <= 3){
-      if (c[rL][cL] === val && c[rL-1][cL+1] === val && c[rL-2][cL+2] === val && c[rL-3][cL+3] === val){
+    while (rL >= 3 && cL <= 3) {
+      if (c[rL][cL] === val && c[rL-1][cL+1] === val && c[rL-2][cL+2] === val && c[rL-3][cL+3] === val) {
         return 1;
       }
-      rL--
-      cL++
+      rL--;
+      cL++;
     }
     return 0;
   }
-  checkHorizontal(row,col){
+  checkHorizontal(row,col) {
     var c = this.state.cells;
     var i = col;
     var val = this.state.player? 2:1;
 
-    while( i >= 3){
-      if(c[row][i] == val && c[row][i-1] == val && c[row][i-2] == val && c[row][i-3] == val){
-        return 1
+    while( i >= 3) {
+      if (c[row][i] == val && c[row][i-1] == val && c[row][i-2] == val && c[row][i-3] == val) {
+        return 1;
       }
-      i--
+      i--;
     }
-    return 0
+    return 0;
   }
-  checkVertical(row, col){
+  checkVertical(row, col) {
     var c = this.state.cells;
     var i = row;
     var val = this.state.player? 2:1;
@@ -141,20 +141,20 @@ class Game extends React.Component {
     while( i >= 3) {
       console.log(c)
       if (c[i][col] == val && c[i-1][col] == val && c[i-2][col] == val && c[i-3][col] == val) {
-        return 1
+        return 1;
       }
-      i--
+      i--;
     }
-    return 0
+    return 0;
   }
-  checkVictory(row, col){
+  checkVictory(row, col) {
     return this.checkVertical(row,col) || this.checkHorizontal(row,col) || this.checkDiagonal(row,col);
   }
 
   // Controls.
-  restart(){
+  restart() {
     var cells = [];
-    for(let i = 0; i < 7; i++ ){
+    for (let i = 0; i < 7; i++ ) {
       cells.push(new Array(7).fill(0));
     }
     this.setState({ player : false, cells : cells, winner:0});
