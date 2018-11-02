0e9490cd5ad92025174d3ffd90687811a47881a4
Lukeveille <luke@lukeleveille.com>
2018-11-01 22:35:24 -0400

    used for scratch pad for module 2 quiz

---
+++ application5.js
@@ -0,0 +1,20 @@
+class Counter extends React.Component{
+    constructor(props){
+        super(props)
+        //initial state set up
+        this.state = {message:"initial message"}
+    }
+    componentDidMount(){
+        //updating state
+        this.setState((prevState, props) => {
+            return {message: prevState.message + '!'}
+        })
+    }
+    render(){
+        return <div>Message:{this.state.message}</div>
+    }
+}
+ReactDOM.render(
+    <Counter />,
+    document.getElementById("root")
+)
